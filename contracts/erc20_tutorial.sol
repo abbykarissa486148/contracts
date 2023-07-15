8c32e69b7aa50d5fee9d75b7023b88f95f430d4c
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-07-15 12:59:14 +0100

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -1,4 +1,7 @@
 pragma solidity ^0.4.24;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'Bell' token contract
+//
+// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED111
+// Symbol      : Bell
