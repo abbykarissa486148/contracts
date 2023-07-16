3d63ea59848d20240635025bc4c75c99960b7fa9
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-07-16 11:52:34 +0100

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -5,3 +5,6 @@ pragma solidity ^0.4.24;
 //
 // Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED111
 // Symbol      : Bell
+// Name        : 0 Fucks Token
+// Total supply: 100000000
+// Decimals    : 18
