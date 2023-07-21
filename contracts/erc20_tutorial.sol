303077617ad89e552c07a95b641c2f8910750b6d
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-07-21 11:45:42 +0100

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -20,3 +20,5 @@ pragma solidity ^0.4.24;
 // ----------------------------------------------------------------------------
 contract SafeMath {
  function safeAdd(uint a, uint b) public pure returns (uint c) {
+ c = a + b;
+        require(c >= a);
