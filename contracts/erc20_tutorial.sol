c6f9e9d1f465d2206c6cf72239394bbc06ab0cb9
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-07-22 15:41:28 +0100

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -22,3 +22,5 @@ contract SafeMath {
  function safeAdd(uint a, uint b) public pure returns (uint c) {
  c = a + b;
         require(c >= a);
+ }
+    function safeSub(uint a, uint b) public pure returns (uint c) {
