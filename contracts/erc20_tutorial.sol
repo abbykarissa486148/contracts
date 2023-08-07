f0c61cc38876f998bde7c5f4cb1bf76b376a9f68
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-08-07 12:39:19 +0100

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -53,3 +53,6 @@ function transferFrom(address from, address to, uint tokens) public returns (boo
     event Transfer(address indexed from, address indexed to, uint tokens);
   event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
+
+// ----------------------------------------------------------------------------
+// Contract function to receive approval and execute function in one call
