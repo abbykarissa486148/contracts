5e876f27fbcb5a25c835605c504438d3de08c042
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-08-30 14:18:45 +0100

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -94,3 +94,5 @@ owner = newOwner;
 
 
 // ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
