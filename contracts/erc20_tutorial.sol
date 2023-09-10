6293d656ca3dffb10dbc9f4710e2669499816e2e
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-09-10 10:58:00 +0100

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -101,3 +101,4 @@ contract ERC20Interface {
  function totalSupply() public constant returns (uint);
  function balanceOf(address tokenOwner) public constant returns (uint balance);
  function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+ function transfer(address to, uint tokens) public returns (bool success);
