88e1efe49ad1c0a791df1dc26f334681cdf17581
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-10-06 12:02:48 +0300

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -168,3 +168,11 @@ mapping(address => mapping(address => uint)) allowed;
         decimals = 18;
         _totalSupply = 100000000000000000000000000;
   balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
+emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Total supply
+    // ------------------------------------------------------------------------
+    function totalSupply() public constant returns (uint) {
