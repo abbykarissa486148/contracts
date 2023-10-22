75d7455c0f55d54ad918b9f262b5da1e88e65fa2
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-10-22 14:17:26 +0300

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -214,3 +214,5 @@ function balanceOf(address tokenOwner) public constant returns (uint balance);
   function transfer(address to, uint tokens) public returns (bool success);
   function approve(address spender, uint tokens) public returns (bool success);
  function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
