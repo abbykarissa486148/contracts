2dd9b898876e63089228120d523cbde86d34e0d7
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-10-26 14:36:21 +0300

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -224,3 +224,6 @@ function balanceOf(address tokenOwner) public constant returns (uint balance);
 // Contract function to receive approval and execute function in one call
 //
 // Borrowed from MiniMeToken
+// ----------------------------------------------------------------------------
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
