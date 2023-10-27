dc24be884e89eb1bab5f9cd4a904572c389b3bf5
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-10-27 15:14:33 +0300

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -227,3 +227,7 @@ function balanceOf(address tokenOwner) public constant returns (uint balance);
 // ----------------------------------------------------------------------------
 contract ApproveAndCallFallBack {
     function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+}
+
+
+// ----------------------------------------------------------------------------
