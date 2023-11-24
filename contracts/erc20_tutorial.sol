6a4349c6f380f25495b1bda0805b96c1d8de7953
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-11-24 15:43:23 +0200

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -309,3 +309,5 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
  // - 0 value transfers are allowed
     // ------------------------------------------------------------------------
     function transfer(address to, uint tokens) public returns (bool success) {
+     balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
