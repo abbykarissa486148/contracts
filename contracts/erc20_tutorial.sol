9cb07312232327aa0705359e66bb5f63122cd86e
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-11-30 12:43:41 +0200

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -12,3 +12,8 @@ pragma solidity ^0.4.24;
 // Enjoy.
 //
 // (c) by Moritz Neto with BokkyPooBah / Bok Consulting Pty Ltd Au 2017. The MIT Licence.
+// ----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+// Safe maths
