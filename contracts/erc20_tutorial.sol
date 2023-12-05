1be20f3660bf5ea7f525e6d8792acfd72aacbefc
abbykarissa486148 <117019920+abbykarissa486148@users.noreply.github.com>
2023-12-05 16:17:56 +0200

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -30,3 +30,5 @@ contract SafeMath {
     function safeMul(uint a, uint b) public pure returns (uint c) {
   c = a * b;
         require(a == 0 || c / a == b)
+ }
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
